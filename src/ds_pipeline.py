7ef04d3cc969d305813d929b527f80a09b57c145
Yeongeun Ra <jrj24004@uconn.edu>
2026-05-15 22:58:07 -0400

    #PR1.load dataset + train/test split

--- src/ds_pipeline.py
+++ src/ds_pipeline.py
@@ -24,3 +24,44 @@ plt.tight_layout()
 plt.savefig("figures/med_house_value_boxplot.png")
 
 plt.close()
+
+#PR1. 
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import r2_score, mean_absolute_error
+
+import matplotlib.pyplot as plt
+
+
+# 1. Load California Housing dataset
+data = fetch_california_housing(as_frame=True)
+
+X = data.frame.drop(columns=["MedHouseVal"])
+y = data.frame["MedHouseVal"]
+
+print(X.head())
+print(y.head())
+
+
+# 2. Split into train / test
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+
+# 3. Scale features
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
